12f201cb8204132e841a08ff83128a77446ef36e
dkrym <dkrym@users.noreply.github.com>
2020-08-08 17:44:43 +1000

    Bootcamp CRUD implementation

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
+const dotenv = require('dotenv'); // env variables
+const morgan = require('morgan'); // logging
+const colors = require('colors'); // colors for console
+const connectDB = require('./config/db'); // mongoDB connection
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to db
+connectDB();
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
 
 const app = express();
+
+// Body parser
+app.use(express.json());
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
@@ -22,6 +30,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 // Run the big boi
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT} in ${process.env.NODE_ENV} mode`);
+const server = app.listen(PORT, () => {
+  console.log(
+    `App listening on port ${PORT} in ${process.env.NODE_ENV} mode`.yellow.bold
+  );
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  server.close(() => process.exit(1));
 });
