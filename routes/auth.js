5f38824c5f65e6366ab7cfc732a1aa51eecab004
dkrym <dkrym@users.noreply.github.com>
2020-08-12 21:26:06 +1000

    Password reset

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,9 @@ const {
   // bring the functions from the file
   register,
   login,
-  getMe
+  getMe,
+  forgotPassword,
+  resetPassword
 } = require('../controllers/auth');
 
 const router = express.Router();
@@ -13,5 +15,7 @@ const { protect } = require('../middleware/auth');
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.post('/forgotpassword', forgotPassword);
+router.put('/resetpassword/:resettoken', resetPassword);
 
 module.exports = router;
