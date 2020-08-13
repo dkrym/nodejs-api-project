e95620864d5b4fe362601bbe60f5b39c2851c59e
dkrym <dkrym@users.noreply.github.com>
2020-08-13 17:30:54 +1000

    Logout

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const {
   // bring the functions from the file
   register,
   login,
+  logout,
   getMe,
   forgotPassword,
   resetPassword,
@@ -16,6 +17,7 @@ const { protect } = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/logout', logout);
 router.get('/me', protect, getMe);
 router.put('/updatedetails', protect, updateDetails);
 router.put('/updatepassword', protect, updatePassword);
